63e3bbd2cb5033a0a76327769f8014a645ee3682
Snehasish NAth <snehasishnath88@gmail.com>
2023-11-03 10:26:43 +0530

    Update

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let mountains_behind = document.getElementById('mountains_behind');
 let text = document.getElementById('text');
-let treeLeft = document.getElementById('tree-left');
-let treeRight = document.getElementById('tree-right');
-let gateLeft = document.getElementById('gate-left');
-let gateRight = document.getElementById('gate-right');
+let btn = document.getElementById('btn');
+let mountains_front = document.getElementById('mountains_front');
+let header=document.querySelector('header');
 
-window.addEventListener('scroll', () =>{
-    let value = window.scrollY;
-    text.style.marginTop = value*2.5 + 'px';
-    treeLeft.style.left = value* -1.5 + 'px';
-    treeRight.style.left = value* 1.5 + 'px';
-    gateLeft.style.left = value* 0.5 +'px';
-    gateRight.style.left = value*-0.5 + 'px';
+window.addEventListener('scroll', function(){
+    let value = this.window.scrollY;
+    stars.style.left = value*0.25 + 'px';
+    moon.style.top = value * 1.05 +'px';
+    mountains_behind.style.top = value* 0.5 + 'px';
+    mountains_front.style.top = value*0 +'px';
+    text.style.marginRight = value*4 + 'px';
+    text.style.marginTop = value*1.5 + 'px';
+    btn.style.marginTop = value*1.5 + 'px';
+    header.style.top = value*0.5 + 'px';
+
 })
