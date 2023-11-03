6de4ed674faa30f9c4368dcf10aaccb8a8eaaefb
Snehasish NAth <snehasishnath88@gmail.com>
2023-11-04 01:45:56 +0530

    Update

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let text = document.getElementById('text');
 let btn = document.getElementById('btn');
 let mountains_front = document.getElementById('mountains_front');
 let header=document.querySelector('header');
+let text1 = document.getElementById('text1')
+let text0 = document.getElementById('text0')
 
 window.addEventListener('scroll', function(){
     let value = this.window.scrollY;
@@ -14,7 +16,30 @@ window.addEventListener('scroll', function(){
     mountains_front.style.top = value*0 +'px';
     text.style.marginRight = value*4 + 'px';
     text.style.marginTop = value*1.5 + 'px';
+    text1.style.marginRight = value*4 + 'px';
+    text1.style.marginTop = value*1.5 + 'px';
+    text0.style.marginLeft = value*10 + 'px';
+    text0.style.marginTop = value*1.5 + 'px';
     btn.style.marginTop = value*1.5 + 'px';
     header.style.top = value*0.5 + 'px';
 
 })
+
+
+
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-links");
+    }
+
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-links")
+    document.getElementById(tabname).classList.add("active-tab")
+
+}
